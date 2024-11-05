b600b04e2f2ee632c9174bae63b1dcf0fccacc5a
Julian Henao Marin <julianhm9612@gmail.com>
2024-11-05 01:24:55 +0000

    Punto 9

---
+++ Punto 9/r.js
@@ -0,0 +1,9 @@
+Los resultados de media y varianza se comparan con los esperados para una distribución uniforme en 
+[0,1], que son 0.5 y 1/12≈0.0833, respectivamente.
+
+Es probable que la media y la varianza calculadas no coincidan exactamente con los valores teóricos debido a
+las variaciones propias de una muestra finita de datos aleatorios. La diferencia puede atribuirse al tamaño
+limitado de la muestra (50 números), lo cual genera una ligera desviación respecto a los valores teóricos de
+una distribución uniforme continua.
+
+Las diferencias se deben a la variabilidad inherente de la muestra y no necesariamente indican que los números no son aleatorios.
